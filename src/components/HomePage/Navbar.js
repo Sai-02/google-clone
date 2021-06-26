1f06433b8057444cc20e3465a27467a449524fc7
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-06-26 16:19:08 +0530

    drop down completed

--- src/components/HomePage/Navbar.js
+++ src/components/HomePage/Navbar.js
@@ -3,11 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import AppsIcon from "@material-ui/icons/Apps";
 import Dropdown from "react-bootstrap/Dropdown";
-import { DropdownButton } from "react-bootstrap";
-import { SplitButton } from "react-bootstrap";
-import { ButtonGroup } from "react-bootstrap";
+import { AccountCircle } from "@material-ui/icons";
 const Navbar = () => {
-  console.log("hello");
   return (
     <nav className="nav">
       <ul className="nav-list">
@@ -34,23 +31,209 @@ const Navbar = () => {
             <Dropdown.Menu className="home-page-drop-down-container">
               <article className="home-page-drop-down">
                 <ul className="drop-down-icons-container">
-                  <li className="drop-down-icon-container">fwfn</li>
-                  <li className="drop-down-icon-container">ht</li>
-                  <li className="drop-down-icon-container">ydryrd</li>
-                  <li className="drop-down-icon-container">1</li>
-                  <li className="drop-down-icon-container">2</li>
-                  <li className="drop-down-icon-container">3</li>
-                  <li className="drop-down-icon-container">4</li>
-                  <li className="drop-down-icon-container">5</li>
-                  <li className="drop-down-icon-container">6</li>
-                  <li className="drop-down-icon-container">7</li>
-                  <li className="drop-down-icon-container">8</li>
-                  <li className="drop-down-icon-container">9</li>
-                  <li className="drop-down-icon-container">10</li>
-                  <li className="drop-down-icon-container">11</li>
-                  <li className="drop-down-icon-container">12</li>
-                  <li className="drop-down-icon-container">13</li>
-                  <li className="drop-down-icon-container">14</li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <AccountCircle style={
+                          {
+                            fontSize:"40px",
+                          }
+                        }/>
+                      </div>
+                      <div className="drop-down-icon-info">Account</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/2-google-search.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Google Search</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a
+                      href="https://www.google.com/business/?ppsrc=GPDA2&gmbsrc=ww-ww-ot-gs-z-gmb-l-z-h~z-ogb-u"
+                      target="_blank"
+                    >
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/3-google-my-business.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">My Business</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a
+                      href="https://www.google.com/maps/search/google+maps/@28.4610277,77.2946795,13z/data=!3m1!4b1"
+                      target="_blank"
+                    >
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/4-google-maps.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Maps</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="https://www.youtube.com/" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/5-youtube.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">YouTube</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a
+                      href="https://play.google.com/store?hl=en"
+                      target="_blank"
+                    >
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/6-google-play.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Play</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a
+                      href="https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en"
+                      target="_blank"
+                    >
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/7-google-news.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">News</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a
+                      href="https://mail.google.com/mail/u/0/#inbox"
+                      target="_blank"
+                    >
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/8-gmail.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Gmail</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="https://meet.google.com/" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/9-google-meet.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Meet</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/10-google-chat.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Chat</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/11-google-contact.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Contacts</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/12-google-drive.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Drive</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/13-google-calendar.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Calendar</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/14-google-translate.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Translate</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/15-google-photos.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Photos</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/16-google-duo.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Duo</div>
+                    </a>
+                  </li>
+                  <li className="drop-down-icon-container">
+                    <a href="#" target="_blank">
+                      <div className="drop-down-icon">
+                        <img
+                          src="./images/navbar/drop-down-menu/17-google-shop.png"
+                          alt="google-icon"
+                        />
+                      </div>
+                      <div className="drop-down-icon-info">Shopping</div>
+                    </a>
+                  </li>
                 </ul>
               </article>
             </Dropdown.Menu>
