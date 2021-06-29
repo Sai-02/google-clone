f27be8c48bf6167865f5043c7772308398912b96
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-06-30 01:43:51 +0530

    minor changes on voice search section

--- src/components/HomePage/VoiceSearch.js
+++ src/components/HomePage/VoiceSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Data } from "../../App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -7,18 +7,42 @@ import MicIcon from "@material-ui/icons/Mic";
 
 const VoiceSearch = () => {
   const { isVoiceSearch, setIsVoiceSearch } = useContext(Data);
+  const [voiceSearchIconContainerClass, setVoiceSearchIconContainerClass] =
+    useState("voice-search-icon-container");
+  const [voiceSearchIconClass, setVoiceSearchIconClass] =
+    useState("voice-search-icon");
   const speech = new SpeechSynthesisUtterance();
   const SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
   const recognition = new SpeechRecognition();
-  recognition.start();
+  // const Listen = () => {
+  // recognition.start();
+  // setTimeout(() => {
+  //   recognition.stop();
+  // }, 5000);
+  // };
+  // Listen();
+
+  recognition.onstart = (e) => {
+    setVoiceSearchIconContainerClass(
+      "voice-search-icon-container background-red"
+    );
+    setVoiceSearchIconClass("voice-search-icon white");
+  };
+  // recognition.onnomatch = () => {
+  //   setVoiceSearchIconContainerClass("voice-search-icon-container");
+  //   setVoiceSearchIconClass("voice-search-icon");
+  // };
   recognition.onresult = (e) => {
+    setVoiceSearchIconContainerClass("voice-search-icon-container");
+    setVoiceSearchIconClass("voice-search-icon");
     console.log(e);
-    speech.text = e.results[0][0].transcript;
+    speech.text = "Getting results, for" + e.results[0][0].transcript;
     speech.volume = 1;
     speech.pitch = 1;
     speech.rate = 1;
     window.speechSynthesis.speak(speech);
+   
   };
   return (
     <section className="voice-search">
@@ -26,6 +50,7 @@ const VoiceSearch = () => {
         <FontAwesomeIcon
           icon={faTimes}
           onClick={() => {
+            recognition.abort();
             setIsVoiceSearch(false);
           }}
           className="voice-search-close-icon"
@@ -34,8 +59,8 @@ const VoiceSearch = () => {
       <article className="voice-search-hero">
         <p>Speak Now</p>
         <div>
-          <div className="voice-search-icon-container">
-            <MicIcon className="voice-search-icon" />
+          <div className={voiceSearchIconContainerClass}>
+            <MicIcon className={voiceSearchIconClass} />
           </div>
         </div>
       </article>
