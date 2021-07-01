7cad50eeb51e05ec0933086a3e89ef2f0f265eba
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-07-01 21:21:54 +0530

    voice search error section added

--- src/components/HomePage/VoiceSearch.js
+++ src/components/HomePage/VoiceSearch.js
@@ -4,6 +4,7 @@ import { Data } from "../../App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import MicIcon from "@material-ui/icons/Mic";
+import VoiceSearchError from "./VoiceSearchError";
 
 const VoiceSearch = () => {
   const { isVoiceSearch, setIsVoiceSearch } = useContext(Data);
@@ -13,65 +14,92 @@ const VoiceSearch = () => {
     "voice-search-icon red"
   );
   const [messsage, setMessage] = useState("Preparing..");
+  const [isVoiceSearchError, setIsVoiceSearchError] = useState(false);
 
-  const speech = new SpeechSynthesisUtterance();
-  const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
-
-  recognition.onstart = (e) => {
-    setVoiceSearchIconClass("voice-search-icon white");
-    setVoiceSearchIconContainerClass(
-      "voice-search-icon-container background-red animation"
-    );
-    setMessage("Listening...");
-  };
-  recognition.onresult = (e) => {
-    setVoiceSearchIconContainerClass("voice-search-icon-container");
-    setVoiceSearchIconClass("voice-search-icon");
-    setMessage(`${e.results[0][0].transcript}...`);
-    console.log(e);
-    recognition.stop();
-    speech.text = "Getting results, for" + e.results[0][0].transcript;
-    speech.volume = 1;
-    speech.pitch = 1;
-    speech.rate = 1;
-    // window.speechSynthesis.speak(speech);
-  };
-  recognition.onend = () => {
-    if (!(messsage === "Listening...")) setMessage("Didn't get that");
-    setVoiceSearchIconContainerClass("voice-search-icon-container");
-    setVoiceSearchIconClass("voice-search-icon");
-  };
+  if (!isVoiceSearchError) {
+    try {
+      var speech = new SpeechSynthesisUtterance();
+      var SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+      var recognition = new SpeechRecognition();
+      window.parent.Listen = () => {
+        recognition.start();
+      };
+      recognition.onstart = (e) => {
+        setVoiceSearchIconClass("voice-search-icon white");
+        setVoiceSearchIconContainerClass(
+          "voice-search-icon-container background-red animation"
+        );
+        setMessage("Listening...");
+      };
+      recognition.onresult = (e) => {
+        setVoiceSearchIconContainerClass("voice-search-icon-container");
+        setVoiceSearchIconClass("voice-search-icon");
+        setMessage(`${e.results[0][0].transcript}...`);
+        console.log(e);
+        recognition.stop();
+        speech.text = "Getting results, for" + e.results[0][0].transcript;
+        speech.volume = 1;
+        speech.pitch = 1;
+        speech.rate = 1;
+        // window.speechSynthesis.speak(speech);
+      };
+      recognition.onend = () => {
+        if (messsage === "Listening...") setMessage("Didn't get that");
+        setVoiceSearchIconContainerClass("voice-search-icon-container");
+        setVoiceSearchIconClass("voice-search-icon");
+      };
+    } catch (e) {
+      setIsVoiceSearchError(true);
+    }
+  }
 
   useEffect(() => {
     setTimeout(() => {
-      recognition.start();
-    }, 3000);
-  }, []);
+      try {
+        window.parent.Listen();
+      } catch (e) {
+        setIsVoiceSearchError(true);
+      }
+    }, 2000);
+  }, [isVoiceSearch]);
+
   return (
     <section className="voice-search">
       <nav className="voice-search-nav">
         <FontAwesomeIcon
           icon={faTimes}
           onClick={() => {
-            recognition.abort();
-            setIsVoiceSearch(false);
+            try {
+              recognition.abort();
+              setIsVoiceSearch(false);
+            } catch (e) {
+              setIsVoiceSearch(false);
+            }
           }}
           className="voice-search-close-icon"
         />
       </nav>
-      <article className="voice-search-hero">
-        <p>
-          {messsage}
-          {/* <span className="try-again-message"> Try again?</span> */}
-        </p>
-        <div>
-          <div className={voiceSearchIconContainerClass}>
-            <MicIcon className={voiceSearchIconClass} />
+      {isVoiceSearchError ? (
+        <VoiceSearchError />
+      ) : (
+        <article className="voice-search-hero">
+          <p>
+            {messsage}
+            {/* <span className="try-again-message"> Try again?</span> */}
+          </p>
+          <div>
+            <div
+              className={voiceSearchIconContainerClass}
+              onClick={() => {
+                recognition.start();
+              }}
+            >
+              <MicIcon className={voiceSearchIconClass} />
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </section>
   );
 };
---
+++ src/components/HomePage/VoiceSearchError.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const VoiceSearchError = () => {
+  return (
+    <section className="voice-search-error">
+      <article className="voice-search-error-section-center">
+        <h1>This functionality does not work in old browsers</h1>
+        <p>switch to newer ones!!!</p>
+      </article>
+    </section>
+  );
+};
+
+export default VoiceSearchError;
