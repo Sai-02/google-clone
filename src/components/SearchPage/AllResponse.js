4779c58ce252b3b8ea3f06c409632d186ff5afb6
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-07-18 00:31:55 +0530

    all response error fix

--- src/components/SearchPage/AllResponse.js
+++ src/components/SearchPage/AllResponse.js
@@ -7,61 +7,67 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 const AllResponse = ({ searchPageRef }) => {
   const { allResponse, isAllResponseFound } = useContext(Data);
   const { searchInformation, items } = allResponse;
- 
-  return (
-    <section className="all-response-container">
-      <div className="space-filler"></div>
-      <article className="all-response">
-        <>
-          {isAllResponseFound ? (
-            <p className="results-info">{`About ${searchInformation.formattedTotalResults} results in (${searchInformation.formattedSearchTime} seconds)`}</p>
-          ) : (
-            <Skeleton variant="text" animated />
-          )}
 
-          <div className="all-response-content-container">
-            {items.map((item) => {
-              return (
+  return (
+    <>
+      <section className="all-response-container">
+        <div className="space-filler"></div>
+        <article className="all-response">
+          <>
+            {isAllResponseFound ? (
+              <p className="results-info">{`About ${searchInformation.formattedTotalResults} results in (${searchInformation.formattedSearchTime} seconds)`}</p>
+            ) : (
+              <Skeleton variant="text" animated />
+            )}
+
+            <div className="all-response-content-container">
+              {isAllResponseFound ? (
                 <>
-                  <div className="all-response-content">
-                    <a
-                      href={item.link}
-                      className="linkToPage"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {isAllResponseFound ? (
-                        <p className="url">
-                          {item.link}{" "}
-                          <span className="all-reponse-icon">
-                            <FontAwesomeIcon icon={faCaretDown} />
-                          </span>
-                        </p>
-                      ) : (
-                        <Skeleton variant="text" animated className="url" />
-                      )}
-                      {isAllResponseFound ? (
-                        <h1 className="title">{item.title}</h1>
-                      ) : (
-                        <Skeleton variant="text" animated className="title" />
-                      )}
-                    </a>
-                    {isAllResponseFound ? (
-                      <p className="snippet">{item.snippet}</p>
-                    ) : (
-                      <Skeleton variant="rect" animated className="snippet" />
-                    )}
-                  </div>
+                  {items.map((item) => {
+                    return (
+                      <>
+                        <div className="all-response-content">
+                          <a
+                            href={item.link}
+                            className="linkToPage"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <p className="url">
+                              {item.link}{" "}
+                              <span className="all-reponse-icon">
+                                <FontAwesomeIcon icon={faCaretDown} />
+                              </span>
+                            </p>{" "}
+                            <h1 className="title">{item.title}</h1>
+                          </a>
+                          <p className="snippet">{item.snippet}</p>
+                        </div>
+                      </>
+                    );
+                  })}
                 </>
-              );
-            })}
-          </div>
-        </>
-      </article>
-      <div className="space-filler"></div>
-      <div className="space-filler"></div>
-      <div className="space-filler"></div>
-    </section>
+              ) : (
+                [...Array(10)].map(() => {
+                  return (
+                    <>
+                      <div className="all-response-content">
+                        <Skeleton variant="text" animated className="url" />
+                        <Skeleton variant="text" animated className="title" />
+                        <Skeleton variant="rect" animated className="snippet" />
+                      </div>
+                    </>
+                  );
+                })
+              )}
+            </div>
+          </>
+        </article>
+        <div className="space-filler"></div>
+        <div className="space-filler"></div>
+        <div className="space-filler"></div>
+      </section>
+    </>
   );
 };
 
