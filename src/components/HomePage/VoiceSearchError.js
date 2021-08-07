4e81014dd6ef7966a5c4e7a9bbffb588bc3d9972
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-08-07 23:32:52 +0530

    VoiceSearchError section updated

--- src/components/HomePage/VoiceSearchError.js
+++ src/components/HomePage/VoiceSearchError.js
@@ -1,11 +1,33 @@
 import React from "react";
+import BrowserNotSupported from "../../images/BrowserNotSupported.png";
 
 const VoiceSearchError = () => {
   return (
     <section className="voice-search-error">
-      <article className="voice-search-error-section-center">
-        <h1>This functionality does not work in old browsers</h1>
-        <p>switch to newer ones!!!</p>
+      <article
+        className="voice-search-error-section-center"
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <div
+          className="img-container"
+          style={{
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <img src={BrowserNotSupported} alt="" />
+        </div>
+        <h1
+          style={{
+            color: "rgb(15, 149, 202)",
+            fontSize: "2rem",
+          }}
+        >
+          This functionality does not work in your browser
+        </h1>
       </article>
     </section>
   );
